abb6b48f06f85d1145076f1147b2af709725a1a9
Mohamed <mohamedabuali97@gmail.com>
2026-04-06 14:01:50 -0500

    Revert "add: added new lines in the print for better reading"
    This reverts commit 793ccab55063e2faf0c0c8516a3a30e6f7465d5e.

--- backend/main.py
+++ backend/main.py
@@ -19,16 +19,3 @@ async def chat_endpoint(request: Request):
         response += chat_response
     return JSONResponse(content={"message": response})
 
-# while True:
-#     try:
-#         input_text = input("\nEnter your message (Enter 'exit' to quit): \n")
-#         if input_text == "exit":
-#             break
-#         add_a_document(input_text)
-#         retrieval = retrieve_document(input_text)
-#         print("\nAssistant:", end="")
-#         for chat_response in chat(input_text, retrieval):
-#             add_a_document(chat_response)
-#             print(chat_response, end="")
-#     except Exception as e:
-#         print(f"Error: {e}")
---
+++ main.py
@@ -0,0 +1,16 @@
+from vectorstore import add_a_document, retrieve_document
+from chat import chat
+
+
+while True:
+    try:
+        input_text = input("\nEnter your message (Enter 'exit' to quit): ")
+        if input_text == "exit":
+            break
+        add_a_document(input_text)
+        retrieval = retrieve_document(input_text)
+        for chat_response in chat(input_text, retrieval):
+            add_a_document(chat_response)
+            print(chat_response, end="")
+    except Exception as e:
+        print(f"Error: {e}")
